2d2596ec921c746916358af972171a74076f7aab
Thiammm <thiamabdoulaye762@gmail.com>
2023-04-18 13:17:39 +0000

    fonctionalité de creation d'un nouveau type d'article

--- app/Http/Livewire/TypeArticleCompenent.php
+++ app/Http/Livewire/TypeArticleCompenent.php
@@ -11,12 +11,48 @@ class TypeArticleCompenent extends Component
     use WithPagination;
     protected $paginationTheme ='bootstrap';
     public $search;
+    public $isBtnClick = "liste";
+    public $newTypeArticle = "";
+
     public function render()
     {
         $searchCritary = '%'.$this->search.'%';
-        return view('livewire.type-article-compenent', ['typearticles' => TypeArticle::
+        return view('livewire.typearticles.index', ['typearticles' => TypeArticle::
         latest('created_at')->where('nom', 'like', $searchCritary)->paginate(5)])
         ->extends('layouts.master')
         ->section('content');
+    }
+
+    public function rules(){
+        return [
+            'newTypeArticle' => 'required|unique:type_articles,nom',
+        ];
+    }
+
+    public function showSuccessMessage($message){
+        $this->dispatchBrowserEvent('showSuccessMessage', [
+            'title' => $message,
+            'timer' => 3000,
+            'icon' => 'success',
+            'toast' => true,
+            'showConfirmButton' => false,
+            'position' => 'top-right',
+        ]);
+    }
+
+    public function goToList(){
+        $this->newTypeArticle = "";
+        $this->isBtnClick = "liste";
+    }
+
+    public function goToAddtype(){
+        $this->isBtnClick = "creer";
+    }
+
+    public function addType(){
+        $donneesValides = $this->validate();
+        TypeArticle::create(['nom' => $donneesValides['newTypeArticle']]);
+        $this->showSuccessMessage("Type d'article crée avec succès");
+        $this->newTypeArticle = "";
     }
 }
